c4e61e71d89cc6a124912d3e48ce1969394c3488
deno101 <dennizwarui@gmail.com>
2020-01-27 11:39:15 +0300

    Create custom app bar and click events for image views

--- app/src/main/java/com/dnz/local/buxs/MainActivity.java
+++ app/src/main/java/com/dnz/local/buxs/MainActivity.java
@@ -2,13 +2,48 @@ package com.dnz.local.buxs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView user,dotsVert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = this.getWindow();
+//        set status bar for sdk > lollipop
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+//        add onclick listeners for image views
+        user = findViewById(R.id.user);
+        dotsVert = findViewById(R.id.dots_vert);
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked User");
+            }
+        });
+
+        dotsVert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked dots-vert menu");
+            }
+        });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
