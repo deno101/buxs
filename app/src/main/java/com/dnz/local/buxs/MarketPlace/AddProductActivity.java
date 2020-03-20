2acaef5c88db491aace7ddf9939c98fb3f5975cc
deno101 <dennizwarui@gmail.com>
2020-03-20 17:17:49 +0300

    Add activity for postint items to backend

---
+++ app/src/main/java/com/dnz/local/buxs/MarketPlace/AddProductActivity.java
@@ -0,0 +1,14 @@
+package com.dnz.local.buxs.MarketPlace;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.dnz.local.buxs.R;
+
+public class AddProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
