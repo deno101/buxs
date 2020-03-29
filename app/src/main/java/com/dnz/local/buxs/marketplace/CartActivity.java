c70912aaa474192e3adb272a9191d0ac76b8ba8b
deno101 <dennizwarui@gmail.com>
2020-03-29 17:18:45 +0300

    create view holder for cart recycler view

---
+++ app/src/main/java/com/dnz/local/buxs/marketplace/CartActivity.java
@@ -0,0 +1,50 @@
+package com.dnz.local.buxs.marketplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.NetworkImageView;
+import com.dnz.local.buxs.MainActivity;
+import com.dnz.local.buxs.R;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.CookieStore;
+
+
+public class CartActivity extends AppCompatActivity {
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        CookieStore cookieStore = MainActivity.getCookieStore();
+        CookieManager cookieManager = new CookieManager(cookieStore, CookiePolicy.ACCEPT_ALL);
+        CookieHandler.setDefault(cookieManager);
+
+        Window window = this.getWindow();
+
+        // set status bar for sdk > lollipop
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+        Network net = new BasicNetwork(new HurlStack());
+
+        requestQueue = new RequestQueue(cache, net,1);
+        requestQueue.start();
+    }
+}
