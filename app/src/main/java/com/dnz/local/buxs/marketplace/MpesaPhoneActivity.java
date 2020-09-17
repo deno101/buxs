838f8001e6525632b7716f0510ca1e4e5817ec2a
deno101 <dennizwarui@gmail.com>
2020-09-17 14:43:03 +0300

    Create layout for mpesa pay

---
+++ app/src/main/java/com/dnz/local/buxs/marketplace/MpesaPhoneActivity.java
@@ -0,0 +1,17 @@
+package com.dnz.local.buxs.marketplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dnz.local.buxs.R;
+
+public class MpesaPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mpesa_phone);
+
+    }
+}
